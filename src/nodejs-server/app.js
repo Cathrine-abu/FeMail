ccaebe0c7263319d61bb73130bd8e524b7ef5c7a
Shira <shira.fisher30@gmail.com>
2025-06-26 18:37:07 +0300

    add web port

--- src/nodejs-server/app.js
+++ src/nodejs-server/app.js
@@ -5,9 +5,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-// Get user arguments
-const args = process.argv.slice(2);
-const port = args[0];
+const PORT = 8080;
 const MONGO_URI = "mongodb://mongo-container:27017/femail-db";
 
 // MongoDB Connection
@@ -41,6 +39,3 @@ app.use('/api/users', userRoutes);           // For user-related endpoints
 app.use('/api/tokens', tokenRoutes);         // For token-related endpoints
 app.use('/api/blacklist', blacklistRoutes);  // For blacklist-related endpoints
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// Start the server and listen for requests
-app.listen(port, () => {});
